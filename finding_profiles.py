060277292e6f723d2ae04c5e2f026e1b748131b6
Jaymin123123 <148958127+Jaymin123123@users.noreply.github.com>
2024-05-28 16:58:42 +0100

    after l2 profile creater is kinda running

--- finding_profiles.py
+++ finding_profiles.py
@@ -4,43 +4,51 @@ import helpers as h
 import matplotlib.pyplot as plt
 import finders_help as fh
 from datetime import datetime, timedelta   
+import spacepy.pycdf as cdf
+import os
 
-start = datetime(2023,6,26)
-end = datetime(2023,6,26)
+start = datetime(2023,1,1)
+end = datetime(2023,12,31)
 duty_cycle = h.load_HK('solo_ANC_sc-thermal-mag', start, end)
-
-cdf_file = '26006/solo_L1_mag-obs-normal_20230626_V01.cdf'
-cdf_file_data = fh.load_l1_data(cdf_file)
-
-duty_cycle_csv = pd.read_csv('duty_cycle.csv')
-
-
+duty_cycle = pd.DataFrame(duty_cycle)
+file_location = '/rds/general/user/js6523/projects/spaceandplanetaryphysics20222025/live/so_mag_heaters/mag_data/solo_L2_mag-rtn-normal-internal/2023'
 threshold = 0.12
 
-# Find the profiles
 def capture_profiles(duty_cycle_csv, threshold):
     segments = []
     in_segment = False
-    for i in range(len(duty_cycle_csv)):
-        if duty_cycle_csv.iloc[i] > threshold:  # Using iloc to access by index position
+    for index, row in duty_cycle_csv.iterrows():
+        if row['IFB_HTR1_LCL5_TM'] > threshold:  # Using iloc to access by index position
             if not in_segment:
-                segments.append(duty_cycle_csv.index[i])
+                segments.append(index)
                 in_segment = True
         else:
             in_segment = False
     return segments
 
+current = start
+while current <= end:
+    cdf_file = f'{file_location}/solo_L2_mag-rtn-normal-internal_{current.strftime("%Y%m%d")}_V00.cdf'
 
+    if not os.path.isfile(cdf_file):
+        print(f"File {cdf_file} does not exist. Skipping to the next date.")
+        current += timedelta(days=1)
+        continue
+    duty_cycle['Date'] = duty_cycle['Time'].dt.date
+    duty_cycle_csv = duty_cycle[duty_cycle['Date'] == current.date()]
+    duty_cycle_csv = duty_cycle_csv.dropna(subset=['IFB_HTR1_LCL5_TM'])
 
+    segments = capture_profiles(duty_cycle_csv, threshold)  # Passing the column directly
+    segments_df = pd.DataFrame(duty_cycle_csv['Time'][segments])
+    segments_df_minus = pd.DataFrame(duty_cycle_csv['Time'][segments]- timedelta(seconds=7))
+    print(duty_cycle)
+    print(segments_df)
+    print(segments_df_minus)
+    print(duty_cycle_csv)
+    # Extract the profiles
+    fh.extract_profiles_l2_10(segments_df,segments_df_minus, cdf_file, 'output_folder', current)
 
-#duty_cycle_csv = duty_cycle_csv['IFB_HTR1_LCL5_TM'].dropna()
-duty_cycle_csv = duty_cycle_csv.dropna(subset=['IFB_HTR1_LCL5_TM'])
+    current += timedelta(days=1)
 
-segments = capture_profiles(duty_cycle_csv['IFB_HTR1_LCL5_TM'], threshold)  # Passing the column directly
-segments_df = pd.DataFrame(duty_cycle_csv['Time'][segments])
-fh.segments_minus_time('segments.csv', 7)
-
-# Extract the profiles
-fh.extract_profiles(segments_df, cdf_file, 'profiles')
 
 
